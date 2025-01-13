219a6f260d3582b19e72842545bcb6253fe9dc03
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2025-01-13 11:43:19 +0100

    change the output of print_argument.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -3,6 +3,6 @@ import sys
 
 if len(sys.argv) > 1:
     for i, arg in enumerate(sys.argv):
-        print(f"Argument {i}: {arg}")
+        print(f"{arg}")
 else:
     print("No arguments were provided.")
